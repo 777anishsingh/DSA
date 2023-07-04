66a7004d38d51ab237a27ba4d0ea50decd8a29dc
777anishsingh <777anish.singh@gmail.com>
2023-07-05 01:41:14 +0530

    Q3

--- 13_recursion/13c_recursion.cpp
+++ 13_recursion/13c_recursion.cpp
@@ -21,6 +21,7 @@ void printPowerSet(string input, int index, string output)
     printPowerSet(input, index + 1, output);
 }
 
+// Q2 combination in a string of digits
 void findCombination(string input, int index, string output)
 {
     if (index == input.length())
@@ -43,7 +44,45 @@ void findCombination(string input, int index, string output)
     }
 }
 
-// Q2 combination in a string of digits
+// Q3 find all even binary equence with same sum of first & second half bits
+
+void findCount(int leftSum, int rightSum, char *arr, int i, int j, int &count)
+{
+    if (i > j)
+    {
+        if (leftSum == rightSum)
+        {
+            cout << "value " << count++ << " ==> " << arr << endl;
+        }
+        return;
+    }
+
+    // 0,0
+    arr[i] = '0';
+    arr[j] = '0';
+    findCount(leftSum, rightSum, arr, i + 1, j - 1, count);
+
+    // 0,1
+    arr[i] = '1';
+    arr[j] = '0';
+    findCount(leftSum + 1, rightSum, arr, i + 1, j - 1, count);
+
+    // 1,0
+    arr[i] = '0';
+    arr[j] = '1';
+    findCount(leftSum, rightSum + 1, arr, i + 1, j - 1, count);
+
+    // 1,1
+    arr[i] = '1';
+    arr[j] = '1';
+    findCount(leftSum + 1, rightSum + 1, arr, i + 1, j - 1, count);
+}
+void compute_value(int n)
+{
+    char *arr = new char[2 * n];
+    int count = 1;
+    return findCount(0, 0, arr, 0, 2 * n - 1, count);
+}
 
 int main()
 {
@@ -52,15 +91,20 @@ int main()
     //  cout << "Enter the String " << endl;
     //  cin >> str;
     //  string output = "";
-
     // printPowerSet(str, 0, output);
 
     // Q2
-    string str;
-    cout << "Enter the Number String " << endl;
-    cin >> str;
-    string output = "";
+    // string str;
+    // cout << "Enter the Number String " << endl;
+    // cin >> str;
+    // string output = "";
+    // findCombination(str, 0, output);
 
-    findCombination(str, 0, output);
+    // Q3
+
+    int n;
+    cin >> n;
+    compute_value(n);
+
     return 0;
 }

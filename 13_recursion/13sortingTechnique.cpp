48ee14638e311c8e538ff74bb81be13e0e2d7ebb
777anishsingh <777anish.singh@gmail.com>
2023-07-06 23:38:38 +0530

    new

--- 13_recursion/13sortingTechnique.cpp
+++ 13_recursion/13sortingTechnique.cpp
@@ -1,6 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+/********************************************************************* MERGE SORT **************************************************************************/
+/*
 void merge(int Originalarr[], int s, int e)
 {
 
@@ -87,4 +89,88 @@ int main()
     {
         cout << originalarr[i] << " ";
     }
+}
+*/
+
+/********************************************************************* COUNT INVERSIONS **************************************************************************/
+// No of inversion most important ==>line no 97 & 112 are important for this
+int inversions = 0; // global variables
+void merge(vector<int> &arr, int low, int mid, int high)
+{
+    vector<int> temp;
+    int left = low;
+    int right = mid + 1;
+
+    while (left <= mid && right <= high)
+    {
+        if (arr[left] <= arr[right])
+        {
+            temp.push_back(arr[left]);
+            left++;
+        }
+        else
+        {
+            temp.push_back(arr[right]);
+            right++;
+            inversions += mid - left + 1; // this is the main line else is merge sort
+        }
+    }
+
+    while (left <= mid)
+    {
+        temp.push_back(arr[left]);
+        left++;
+    }
+
+    while (right <= high)
+    {
+        temp.push_back(arr[right]);
+        right++;
+    }
+
+    for (int i = low; i <= high; i++)
+    {
+        arr[i] = temp[i - low];
+    }
+}
+
+void mergeSort(vector<int> &arr, int low, int high)
+{
+    if (low >= high)
+        return;
+    int mid = (low + high) / 2;
+    mergeSort(arr, low, mid);
+    mergeSort(arr, mid + 1, high);
+    merge(arr, low, mid, high);
+}
+
+int numberOfInversions(vector<int> &arr, int n)
+{
+    mergeSort(arr, 0, n - 1);
+    return inversions;
+}
+
+int main()
+{
+
+    vector<int> arr;
+    arr.push_back(22);
+    arr.push_back(22);
+    arr.push_back(16);
+    arr.push_back(28);
+    arr.push_back(15);
+    arr.push_back(19);
+    arr.push_back(1);
+    arr.push_back(16);
+    arr.push_back(7);
+    arr.push_back(2);
+    arr.push_back(1);
+    arr.push_back(10);
+    arr.push_back(17);
+    arr.push_back(28);
+    arr.push_back(25);
+
+    cout << "No of inversions are - " << numberOfInversions(arr, arr.size());
+
+    return 0;
 }
